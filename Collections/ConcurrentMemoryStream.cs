db259fb111fa0dc1fee2e8df76716edadb8475b0
WODICHKA <nikita.akulinin@yandex.ru>
2020-05-22 10:11:31 +0300

    optimized work with memory

--- Collections/ConcurrentMemoryStream.cs
+++ Collections/ConcurrentMemoryStream.cs
@@ -26,7 +26,7 @@ namespace WFast.Collections
             _readptr = _hndl;
             _endPtr = _hndl + _bufferSize;
 
-            zeromemoryimpl((byte*)_hndl, _bufferSize);
+            new Span<byte>((void*)_hndl, _bufferSize).Fill(0);
 
         }
 
@@ -34,111 +34,6 @@ namespace WFast.Collections
         {
             Console.WriteLine("~ConcurrentMemoryStream()");
             Marshal.FreeHGlobal(_hndl);
-        }
-
-        internal static unsafe void memcpyimpl(byte* src, byte* dest, int len)
-        {
-            if (len == 1)
-                *dest = *src;
-            else if ((len & 0x01) != 0)
-            {
-                ___memcpyimpl(src, dest, len - 1);
-                *(dest + (len - 1)) = *(src + (len - 1));
-            }
-            else
-                ___memcpyimpl(src, dest, len);
-        }
-        internal static unsafe void zeromemoryimpl(byte* dest, int len)
-        {
-            if (len == 1)
-                *dest = 0;
-            else if ((len & 0x01) != 0)
-            {
-                ___zeroMemory(dest, len - 1);
-                *(dest + (len - 1)) = 0;
-            }
-            else
-                ___zeroMemory(dest, len);
-        }
-        internal static unsafe void ___zeroMemory(byte* dest, int len)
-        {
-            if (len >= 0x10)
-            {
-                do
-                {
-                    *((long*)dest) = 0;
-                    *((long*)(dest + 8)) = 0;
-                    dest += 0x10;
-                }
-                while ((len -= 0x10) >= 0x10);
-            }
-            if (len > 0)
-            {
-                if ((len & 8) != 0)
-                {
-                    *((long*)dest) = 0;
-                    dest += 8;
-                }
-                if ((len & 4) != 0)
-                {
-                    *((int*)dest) = 0;
-                    dest += 4;
-                }
-                if ((len & 2) != 0)
-                {
-                    *((short*)dest) = 0;
-                    dest += 2;
-                }
-                if ((len & 1) != 0)
-                {
-                    dest++;
-                    dest[0] = 0;
-                }
-            }
-        }
-        internal static unsafe void ___memcpyimpl(byte* src, byte* dest, int len)
-        {
-            if (len >= 0x10)
-            {
-                do
-                {
-                    *((long*)dest) = *((long*)src);
-                    //*((int*)(dest + 4)) = *((int*)(src + 4));
-                    *((long*)(dest + 8)) = *((long*)(src + 8));
-                    // *((int*)(dest + 12)) = *((int*)(src + 12));
-                    dest += 0x10;
-                    src += 0x10;
-                }
-                while ((len -= 0x10) >= 0x10);
-            }
-            if (len > 0)
-            {
-                if ((len & 8) != 0)
-                {
-                    *((long*)dest) = *((long*)src);
-                    //    *((int*)(dest + 4)) = *((int*)(src + 4));
-                    dest += 8;
-                    src += 8;
-                }
-                if ((len & 4) != 0)
-                {
-                    *((int*)dest) = *((int*)src);
-                    dest += 4;
-                    src += 4;
-                }
-                if ((len & 2) != 0)
-                {
-                    *((short*)dest) = *((short*)src);
-                    dest += 2;
-                    src += 2;
-                }
-                if ((len & 1) != 0)
-                {
-                    dest++;
-                    src++;
-                    dest[0] = src[0];
-                }
-            }
         }
 
         public void DoubleWrite(byte *bf_1, int bf_1Size, byte *bf_2, int bf_2Size)
@@ -166,8 +61,8 @@ namespace WFast.Collections
                 sw.SpinOnce();
             } while (true);
 
-            memcpyimpl((byte*)bf_1, (byte*)(head + _controlInfoSize), bf_1Size);
-            memcpyimpl((byte*)bf_2, (byte*)(head + _controlInfoSize + bf_1Size), bf_2Size);
+            Buffer.MemoryCopy((byte*)bf_1, (byte*)(head + _controlInfoSize), bf_1Size, bf_1Size);
+            Buffer.MemoryCopy((byte*)bf_2, (byte*)(head + _controlInfoSize + bf_1Size), bf_2Size, bf_2Size);
             Interlocked.MemoryBarrier();
             *(int*)head = (int)(count_bytes | 0x80000000);
         }
@@ -194,9 +89,8 @@ namespace WFast.Collections
                 sw.SpinOnce();
             } while (true);
 
-            
 
-            memcpyimpl((byte*)buffer, (byte*)(head + _controlInfoSize), count_bytes);
+            Buffer.MemoryCopy(buffer,(byte*)(head + _controlInfoSize), count_bytes, count_bytes);
             Interlocked.MemoryBarrier();
             *(int*)head = (int)(count_bytes | 0x80000000);
             
@@ -231,8 +125,9 @@ namespace WFast.Collections
             } while (true);
             
             control_info &= 0x7fff;
-            memcpyimpl((byte*)(_readptr + _controlInfoSize), _OutPut, control_info);
-            zeromemoryimpl((byte*)_readptr, control_info + _controlInfoSize);
+
+            Buffer.MemoryCopy((byte*)(_readptr + _controlInfoSize), _OutPut, control_info, control_info);
+            new Span<byte>((byte*)_readptr, control_info + _controlInfoSize).Fill(0);
             _readptr += control_info + _controlInfoSize;
             return control_info;
         }
